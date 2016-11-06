555eb9428014c32bc879e0c715c53acef8fa5440
WTobor <weronika.tobor@op.pl>
2016-11-06 14:53:54 +0100

    added bootstrap and external js file for controllers

---
+++ BoardGamesRoom/ViewScripts/BoardGames.js
@@ -0,0 +1,24 @@
+﻿
+function GetAllBoardGames() {
+    jQuery.support.cors = true;
+    $.ajax({
+        url: 'api/boardGames',
+        type: 'GET',
+        dataType: 'json',
+        success: function (data) {
+            WriteResponseBG(data);
+        },
+        error: function (x, y, z) {
+            alert(x + '\n' + y + '\n' + z);
+        }
+    });
+}
+
+function WriteResponseBG(boardGames) {
+    var strResult = "<table><th>Nazwa gry</th>";
+    $.each(boardGames, function (index, boardGame) {
+        strResult += "<tr><td>" + boardGame.Name + "</td></tr>";
+    });
+    strResult += "</table>";
+    $("#divBody").html(strResult);
+}
---
+++ BoardGamesRoom/ViewScripts/Users.js
@@ -0,0 +1,24 @@
+﻿
+function GetAllUsers() {
+    jQuery.support.cors = true;
+    $.ajax({
+        url: 'api/users',
+        type: 'GET',
+        dataType: 'json',
+        success: function (data) {
+            WriteResponseU(data);
+        },
+        error: function (x, y, z) {
+            alert(x + '\n' + y + '\n' + z);
+        }
+    });
+}
+
+function WriteResponseU(users) {
+    var strResult = "<table><th>Nazwa użytkownika</th>";
+    $.each(users, function (index, user) {
+        strResult += "<tr><td>" + user.Login + "</td></tr>";
+    });
+    strResult += "</table>";
+    $("#divBody").html(strResult);
+}
